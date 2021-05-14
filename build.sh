489398abe8076ca56c92c2c6fdfbea3693b9142e
Ju Ouyang <juouayang@aicots.com>
2021-05-14 15:17:21 +0800

    Updates: 1. for vallyexpressselect 2. fix requirements.txt 3. extract variables

--- build.sh
+++ build.sh
@@ -1,52 +1,39 @@
 #!/bin/bash
 
 TIMESTAMP=$(date +"%Y%m%d%H%M%S.%3N")
+POSTFIX=$RANDOM
+
 PYTHON_VERSION='3.8'
+STRATEGY_NAME=$(basename "$PWD")
 
-#
-: <<'END'
-userID/strategyID/
-.
-├── __main__.py
-├── Your_Strategy.py
-├── reference/
-└── .staging
-    ├── logs
-    └── run.sh
-END
-#
-
-STRATEGY_NAME=$(ls -A1 *.py | grep -v __main__ | sed -e 's/\.py$//') # Your_Strategy
-APP_NAME=$(basename $(dirname "$PWD"))"/"$(basename "$PWD") # userID/strategyID
-APP_VERSION=$TIMESTAMP
-APP_ENTRYPOINT="__main__.py"
-SOURCE_DIR='.'
-
-if [ ! -z "$JENKINS_HOME" ]; then
-    ### JENKINS
-    APP_NAME=$JOB_NAME"/"$STRATEGY_NAME
-    APP_VERSION="r_$SVN_REVISION"
+# check entrypoint
+if [ -f "__main__.py" ] && [ "$(ls -la *.py | grep -v __main__.py | wc -l)" == "1"  ]; then
+  # strategy template
+  APP_ENTRYPOINT="__main__.py"
+elif [ -f "__main__.py" ]; then
+  APP_ENTRYPOINT="__main__.py"
+else
+  echo -n "Input filename.py of entrypoint: "
+  read APP_ENTRYPOINT
+  APP_ENTRYPOINT="${APP_ENTRYPOINT:=ValleyExpressSelect.py}"
 fi
+if [ ! -f "$APP_ENTRYPOINT" ]; then echo "Entrypoint: $APP_ENTRYPOINT not found" && exit 300; fi
 
-DOCKER_REPOSITORY=$APP_NAME
+# check docker
+if [ ! -x "$(command -v docker)" ]; then echo "Access https://docs.docker.com/engine/install/, and install docker first." && exit 301; fi
+DOCKER_REPOSITORY=$(basename $(dirname "$PWD"))"/"$(basename "$PWD") # userID/strategyID
+DOCKER_REPOSITORY=$DOCKER_REPOSITORY
 DOCKER_REPOSITORY="${DOCKER_REPOSITORY,,}"
 DOCKER_REPOSITORY="${DOCKER_REPOSITORY// /_}"
-DOCKER_TAG=$APP_VERSION
+DOCKER_TAG=$TIMESTAMP
 DOCKER_TAG="${DOCKER_TAG,,}"
 CONTAINER_NAME=$(basename $(dirname "$PWD"))"-"$(basename "$PWD") # userID-strategyID
 
-
-cd $SOURCE_DIR
-if [ ! -f "$SOURCE_DIR/$APP_ENTRYPOINT" ]; then echo "Entrypoint: $SOURCE_DIR/$APP_ENTRYPOINT not found" && exit 300; fi
-if [ ! -x "$(command -v docker)" ]; then echo "Access https://docs.docker.com/engine/install/, and install docker first." && exit 301; fi
-
-POSTFIX=$RANDOM
-
+# generate .dockerignore
 if [ -f ".dockerignore" ]; then mv -f .dockerignore .dockerignore.$POSTFIX.bak; fi
 cat <<EOF > .dockerignore
 **/.staging
-**/log
-**/logs
+**/log*
 **/.svn
 **/branches
 **/tags
@@ -71,6 +58,7 @@ docker-compose*
 .vscode
 EOF
 
+# generate requirements.txt
 cat <<EOF > requirements.$POSTFIX.txt
 schedule==1.0.0
 lineTool==1.0.3
@@ -80,82 +68,149 @@ requests==2.22.0
 paho-mqtt==1.5.1
 getmac==0.8.2
 EOF
+if [ $(grep -inr --include \*.py -R "matplotlib" | wc -l) -ne 0 ]; then echo "matplotlib==3.4.1" >> requirements.$POSTFIX.txt; fi
+if [ $(grep -inr --include \*.py -R "shioaji" | wc -l) -ne 0 ]; then echo "shioaji==0.3.1.dev8" >> requirements.$POSTFIX.txt; fi
+if [ $(grep -inr --include \*.py -R "Crypto" | wc -l) -ne 0 ]; then echo "pycryptodome==3.10.1" >> requirements.$POSTFIX.txt; fi
 
-if grep -inr --include \*.py -R "matplotlib";   then echo "matplotlib==3.4.1" >> requirements.$POSTFIX.txt;      fi
-if grep -inr --include \*.py -R "shioaji";      then echo "shioaji==0.3.1.dev8" >> requirements.$POSTFIX.txt;    fi
-if grep -inr --include \*.py -R "Crypto";       then echo "pycryptodome==3.10.1" >> requirements.$POSTFIX.txt;   fi
+# generate wrapper_script.sh
+if [ $APP_ENTRYPOINT = "ValleyExpressSelect.py" ]; then
+cat <<EOF > my_wrapper_script.sh
+#!/bin/bash
+python $APP_ENTRYPOINT
+while true
+do
+  currentTime=\$(TZ=":Asai/Taipei" date +"%H%M")
+  echo -n .
+  if [ "\$currentTime" == "0600" ]; then
+    echo "Triggered at "\$currentTime
+    RC=1
+    while [ \$RC -ne 0 ]
+    do
+      python $APP_ENTRYPOINT
+      RC=\$?
+      echo "RC="\$RC
+      if [ \$RC != 0 ]; then
+        echo "Failed"
+      fi
+    done
+  fi
+  sleep 60
+done
+EOF
+else
+cat <<EOF > my_wrapper_script.sh
+#!/bin/bash
+python $APP_ENTRYPOINT
+EOF
+fi
+chmod +x my_wrapper_script.sh
 
-#TZ=$(cat /etc/timezone 2>/dev/null)
-TZ="${TZ:=Asia/Taipei}"
-
+# generate Dockerfile
+TZ="${TZ:=UTC}"
 cat <<EOF > Dockerfile.$POSTFIX
 FROM python:$PYTHON_VERSION-slim
 USER root
-COPY $SOURCE_DIR/requirements.$POSTFIX.txt /tmp/requirements.txt
+COPY requirements.$POSTFIX.txt /tmp/requirements.txt
 RUN pip install -r /tmp/requirements.txt \
  && mkdir -p /builds/app
-COPY $SOURCE_DIR/ /builds/app/
+COPY ./ /builds/app/
 WORKDIR /builds/app
 ENV TZ "$TZ"
-ENTRYPOINT ["python", "$APP_ENTRYPOINT"]
+COPY my_wrapper_script.sh my_wrapper_script.sh
+CMD bash
+ENTRYPOINT ./my_wrapper_script.sh
 EOF
 
-
+# docker build
 sudo docker rmi $DOCKER_REPOSITORY:$DOCKER_TAG &> /dev/null
-sudo docker build -t $DOCKER_REPOSITORY:$DOCKER_TAG --file ./Dockerfile.$POSTFIX . # | sed -nr '/^Step|tagged/p'
+sudo docker build -t $DOCKER_REPOSITORY:$DOCKER_TAG --file ./Dockerfile.$POSTFIX . | sed -nr '/^Step|tagged/p'
 BUILD_RC=$?
 rm -rf "Dockerfile.$POSTFIX"
 rm -rf "requirements.$POSTFIX.txt"
 rm -rf ".dockerignore"; if [ -f ".dockerignore.$POSTFIX.bak" ]; then mv -f .dockerignore.$POSTFIX.bak .dockerignore; fi
 
-
+# generate staging folder
 STAGING_DIR="./.staging"
-mkdir -p $STAGING_DIR"/logs"
+mkdir -p $STAGING_DIR"/log"
 chmod -R 777 $STAGING_DIR
 
-if [ -f "./TradeBot.py" ]; then
-
-mkdir -p $STAGING_DIR"/AccountPassword"
-cat <<EOF > $STAGING_DIR"/run.sh"
-docker load < "$STRATEGY_NAME-$TIMESTAMP.tar.gz"
-docker run --rm -it \
--e MQTT_IP=192.168.233.134 \
--e MQTT_PORT=1883 \
--v \$(pwd)/logs/:/builds/app/logs \
--v \$(pwd)/AccountPassword/Config.json:/builds/app/reference/Config.json \
--v \$(pwd)/AccountPassword/private_key.pem:/builds/app/reference/private_key.pem \
--v \$(pwd)/AccountPassword/Sinopac.pfx:/builds/app/reference/Sinopac.pfx \
---name $CONTAINER_NAME \
-$DOCKER_REPOSITORY:$DOCKER_TAG
-EOF
-
+# generate run.sh
+if [ $(grep -inr --include \*.py -R "'logs'" | wc -l) -ne 0 ]; then
+  LOG_DIR="logs";
 else
-
+  LOG_DIR="log"
+fi
 cat <<EOF > $STAGING_DIR"/run.sh"
+if [ \$(ip addr | grep 192.168.233 | wc -l) -ne 0 ]; then
+  MQTT_IP=192.168.233.134
+  MQTT_PORT=1883
+  VOLUME_DEVICE="//192.168.233.190/Knowledge"
+  VOLUME_OPT_O="username=dl.chiu,password=chiu123456,vers=1.0"
+elif [ \$(ip addr | grep 192.168.238 | wc -l) -ne 0 ]; then
+  MQTT_IP=192.168.238.100
+  MQTT_PORT=5883
+  VOLUME_DEVICE="//192.168.238.100/rep"
+  VOLUME_OPT_O="username=jetson_nano,password=jn@123456"
+fi
+EOF
+if [ -f "./TradeBot.py" ]; then
+mkdir -p $STAGING_DIR"/AccountPassword"
+cat <<EOF >> $STAGING_DIR"/run.sh"
 docker load < "$STRATEGY_NAME-$TIMESTAMP.tar.gz"
 docker run --rm -it \
--e MQTT_IP=192.168.233.134 \
--e MQTT_PORT=1883 \
--v \$(pwd)/logs/:/builds/app/logs \
---name $CONTAINER_NAME \
-$DOCKER_REPOSITORY:$DOCKER_TAG
+  -e MQTT_IP=\$MQTT_IP \
+  -e MQTT_PORT=\$MQTT_PORT \
+  -v \$(pwd)/$LOG_DIR/:/builds/app/$LOG_DIR \
+  -v \$(pwd)/AccountPassword/Config.json:/builds/app/reference/Config.json \
+  -v \$(pwd)/AccountPassword/private_key.pem:/builds/app/reference/private_key.pem \
+  -v \$(pwd)/AccountPassword/Sinopac.pfx:/builds/app/reference/Sinopac.pfx \
+  --name $CONTAINER_NAME \
+  $DOCKER_REPOSITORY:$DOCKER_TAG
 EOF
-
+elif [ -f "./ValleyExpressSelect.py" ]; then
+cat <<EOF >> $STAGING_DIR"/run.sh"
+docker load < "$STRATEGY_NAME-$TIMESTAMP.tar.gz"
+docker volume rm rep
+docker volume create \
+  --driver local \
+  --opt type=cifs \
+  --opt device=\$VOLUME_DEVICE \
+  --opt o=\$VOLUME_OPT_O \
+  --name rep
+docker volume inspect rep
+docker run --rm -it \
+  -e MQTT_IP=\$MQTT_IP \
+  -e MQTT_PORT=\$MQTT_PORT \
+  -v \$(pwd)/$LOG_DIR/:/builds/app/$LOG_DIR \
+  -v rep:/builds/rep \
+  --name $CONTAINER_NAME \
+  $DOCKER_REPOSITORY:$DOCKER_TAG
+EOF
+else # strategy
+cat <<EOF >> $STAGING_DIR"/run.sh"
+docker load < "$STRATEGY_NAME-$TIMESTAMP.tar.gz"
+docker run --rm -it \
+  -e MQTT_IP=\$MQTT_IP \
+  -e MQTT_PORT=\$MQTT_PORT \
+  -v \$(pwd)/$LOG_DIR/:/builds/app/$LOG_DIR \
+  -v rep:/builds/rep \
+  --name $CONTAINER_NAME \
+  $DOCKER_REPOSITORY:$DOCKER_TAG
+EOF
 fi
 
-
 # save docker image
-DEST_DIR=$STAGING_DIR
 if [ ! -z "$JENKINS_HOME" ]; then
-    ### JENKINS
-    DEST_DIR=/media/nfs/jenkins/$JOB_NAME
+  ### JENKINS
+  DEST_DIR=/media/nfs/jenkins/$JOB_NAME
+else
+  DEST_DIR=$STAGING_DIR
 fi
 echo saving docker image ...
 sudo rm -rf $DEST_DIR/$STRATEGY_NAME-$TIMESTAMP.tar.gz
 sudo mkdir -p $DEST_DIR
 sudo chmod -R 777 $DEST_DIR
 time sudo docker save $DOCKER_REPOSITORY:$DOCKER_TAG | gzip > $DEST_DIR/$STRATEGY_NAME-$TIMESTAMP.tar.gz
-
 
 # test run to check if app can run for more than 9 seconds
 TESTRUN_NAME=testrun-$POSTFIX-$TIMESTAMP
@@ -171,8 +226,5 @@ fi
 sudo docker stop $TESTRUN_NAME
 sudo docker rmi $DOCKER_REPOSITORY:$DOCKER_TAG
 
-#if [ "$BUILD_RC" -eq "0" ]; then
-    echo "Build complete with return code "$BUILD_RC
-#fi
-
+echo "Build complete with return code "$BUILD_RC
 exit $BUILD_RC
